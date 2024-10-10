f209126cde25e31c61d65d9a0a349036c4804088
Baek In-Chang <inchang.baek@gm.gist.ac.kr>
2024-10-10 11:48:17 -0400

    add reward code tips

--- pcgrllm/utils/exceptions.py
+++ pcgrllm/utils/exceptions.py
@@ -3,7 +3,7 @@ class RewardParsingException(Exception):
     def __init__(self, code, message):
         self.code = code
         self.message = message
-
+        self.time = 'Error while parsing reward function from text'
 
     def __str__(self):
         # split lines
@@ -13,14 +13,14 @@ class RewardParsingException(Exception):
         for i, line in enumerate(lines):
             lined_code += f'{i+1} {line}\n'
 
-        return f'\n[Code]\n{lined_code}\n[Message]\n{self.message}'
+        return f'\nT[Code]\n{lined_code}\n[Message]\n{self.time}\n{self.message}'
 
 
 class RewardExecutionException(Exception):
     def __init__(self, code, message):
         self.code = code
         self.message = message
-
+        self.time = 'Run-time error while executing reward function on the environment'
 
     def __str__(self):
         # split lines
@@ -30,4 +30,4 @@ class RewardExecutionException(Exception):
         for i, line in enumerate(lines):
             lined_code += f'{i+1} {line}\n'
 
-        return f'\n[Code]\n{lined_code}\n[Message]\n{self.message}'
+        return f'\n[Code]\n{lined_code}\n[Message]\n{self.time}\n{self.message}'
