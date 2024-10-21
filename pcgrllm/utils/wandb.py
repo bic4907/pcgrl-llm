7e155709806d95c0d8eab1ab96d029cbf7700884
Baek In-Chang <inchang.baek@gm.gist.ac.kr>
2024-10-20 20:19:46 -0400

    update dockerfiles

--- pcgrllm/utils/wandb.py
+++ pcgrllm/utils/wandb.py
@@ -12,11 +12,13 @@ def start_wandb(config: Config, iteration: int):
     config.exp_dir = path.join(config.exp_dir, f'iteration_{iteration}')
     config.current_iteration = iteration
 
-    wandb.init(
-        project=config.wandb_project,
-        name=get_wandb_name(config),
-        save_code=True)
-    wandb.config.update(dict(config), allow_val_change=True)
+    if config.wandb_key and config.wandb_project:
+        wandb.login(key=config.wandb_key)
+        wandb.init(
+            project=config.wandb_project,
+            name=get_wandb_name(config),
+            save_code=True)
+        wandb.config.update(dict(config), allow_val_change=True)
 
 
 def finish_wandb():
--- run_docker.sh
+++ run_docker.sh
@@ -1,58 +1,17 @@
 #!/bin/bash
 
-# 외부에서 받은 인자들 (공백으로 구분된 key=value 쌍)
-INPUT_ARGS="$@"
+# CUDA 버전 확인 (nvidia-smi 사용)
+cuda_version=$(nvidia-smi | grep -oP "CUDA Version: \K[0-9]+")
 
-# 콤마로 구분된 key=value 쌍을 파싱
-args_array=()
-for kv_pair in "$@"; do
-    args_array+=("$kv_pair")
-done
-
-# exp_name 생성 (모든 key=val 형식, 각 인자값 사이에 _로 구분)
-exp_name=""
-for arg in "${args_array[@]}"; do
-    exp_name+="${arg}_"
-done
-
-# 마지막 _ 제거 (substring expression 수정)
-exp_name=$(echo $exp_name | sed 's/_$//')
-
-echo "Running experiment with exp_name=$exp_name on GPU"
-
-# Python 명령 생성 (exp_name이 존재할 경우에만 추가)
-if [[ -n "$exp_name" ]]; then
-    python_command="python experiment.py ${INPUT_ARGS//,/ } exp_name=$exp_name"
+# 도커 이미지 선택
+if [ "$cuda_version" -eq 12 ]; then
+    docker_image="bic4907/pcgrl:cu12"
+elif [ "$cuda_version" -eq 11 ]; then
+    docker_image="bic4907/pcgrl:cu11"
 else
-    python_command="python experiment.py ${INPUT_ARGS//,/ }"
+    echo "Unsupported CUDA version: $cuda_version"
+    exit 1
 fi
 
-# Python 실행 명령을 출력
-echo "Python command to be executed: $python_command"
-
-# sbatch가 있는지 확인하고 없으면 그냥 명령을 출력하고 종료
-if command -v sbatch &> /dev/null; then
-    # sbatch 옵션을 동적으로 추가하여 job-name 설정
-    sbatch --job-name=$exp_name --output=output_logs/output_%A_%a.out --error=error_logs/error_%A_%a.err <<-EOF
-#!/bin/bash
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:1
-#SBATCH --mem=16G
-#SBATCH --time=1-00:00:00
-#SBATCH --array=1-3
-#SBATCH --account=pr_174_general
-
-# 환경변수 설정
-export LOG_LEVEL=DEBUG
-export XLA_PYTHON_CLIENT_PREALLOCATE=false
-
-# Python 스크립트 실행 (파싱된 모든 인자 전달)
-echo "Executing: $python_command"
-$python_command
-EOF
-else
-    echo "sbatch not found, showing command instead of execution:"
-    echo "#SBATCH --job-name=$exp_name --output=output_logs/output_%A_%a.out --error=error_logs/error_%A_%a.err"
-    echo "Executing the following command instead: $python_command"
-fi
+# 도커 컨테이너 실행
+docker run --rm -it --gpus all -v $(pwd):/app -v /mnt/nas:/mnt/nas --network host $docker_image /bin/bash
