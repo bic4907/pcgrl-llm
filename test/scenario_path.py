6deacf03dcf85c4866945618783960f9f89b7ab5
Baek In-Chang <inchang.baek@gm.gist.ac.kr>
2024-11-15 16:15:06 +0900

    Add `calc_path_from_a_to_b` flood algorithm

--- test/scenario_path.py
+++ test/scenario_path.py
@@ -1,6 +1,6 @@
 import jax.numpy as jnp
 
-from envs.pathfinding import FloodPath, calc_path_from_a_to_b, calc_path_length
+from envs.pathfinding import FloodPath, calc_path_from_a_to_b, calc_path_length, get_path_coords
 
 # Example map (16x16)
 env_map = jnp.array([
@@ -26,6 +26,14 @@ flood_path_net.init_params(env_map.shape)
 #     flood_path_net, env_map, passable_tiles, src, trg
 # )
 
-path_length, flood_count, path_coords = calc_path_from_a_to_b(
+path_length, flood_count, nearest_trg_xy = calc_path_from_a_to_b(
     flood_path_net, env_map, passable_tiles, src, trg
 )
+
+
+print('path_length', path_length)
+print('flood_count', flood_count)
+print('nearest_trg_xy', nearest_trg_xy)
+
+path_coords = get_path_coords(flood_count=flood_count, max_path_len=int(path_length), coord1=trg)
+print('path_coords', path_coords)
