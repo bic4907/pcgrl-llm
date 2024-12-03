851ceae6925349e2b27226a268f1919dcc5b3166
k-shyun <st4889ha@gmail.com>
2024-12-03 14:42:39 +0900

    Modify: pk_flood_state in check_event_jit

--- envs/pathfinding.py
+++ envs/pathfinding.py
@@ -486,6 +486,19 @@ def check_event_jit(
     pk_path_length, pk_flood_state, _ = calc_path_from_a_to_b(env_map, passable_tiles, src, key)
     kd_path_length, kd_flood_state, _ = calc_path_from_a_to_b(env_map, passable_tiles, key, trg)
 
+    # Erase key argument from passable_tiles
+    non_keys_passable_tiles = jnp.array([1, 3, 4, 5, 6, 8])
+    k_y, k_x = key
+    copy_map = env_map.at[k_y, k_x].set(1)
+    non_keys_passable_pk_path_length, non_keys_passable_pk_flood_state, _ = calc_path_from_a_to_b(copy_map, non_keys_passable_tiles, src, key)
+
+    pk_flood_state = jax.lax.cond(
+        pk_path_length == non_keys_passable_pk_path_length,  # Condition
+        lambda _: non_keys_passable_pk_flood_state,  # If true
+        lambda _: pk_flood_state,  # If false
+        operand=None  # Dummy operand (not used)
+    )
+
     # Condition to check valid paths
     valid_paths = (pk_path_length > 0) & (kd_path_length > 0)
 
@@ -536,8 +549,8 @@ def check_event_jit(
 def check_monster(env_map: chex.Array, route_path: chex.Array) -> Tuple[int, chex.Array, chex.Array]:
 
     max_path_len = get_max_path_length_static(map_shape=env_map.shape)
-    dx = [-1, 0, 1, 0, 0]
-    dy = [0, -1, 0, 1, 0]
+    dx = [-1, 0, 1, 0]
+    dy = [0, -1, 0, 1]
 
     monster = {}
     monster_types = jnp.array([4, 5, 6])
@@ -579,8 +592,8 @@ def check_monster(env_map: chex.Array, route_path: chex.Array) -> Tuple[int, che
 partial(jit, static_argnums=(0, 1))
 def check_monster_jit(env_map: chex.Array, route_path: chex.Array) -> chex.Array:
     max_path_len = env_map.shape[0]  # Assuming square map
-    dx = jnp.array([-1, 0, 1, 0, 0])
-    dy = jnp.array([0, -1, 0, 1, 0])
+    dx = jnp.array([-1, 0, 1, 0])
+    dy = jnp.array([0, -1, 0, 1])
 
     monster_types = jnp.array([4, 5, 6], dtype=jnp.int32)  # Define monster types
 
@@ -697,44 +710,6 @@ def check_monster_jit(env_map: chex.Array, route_path: chex.Array) -> chex.Array
 
     return jnp.array([bat_count, scorpion_count, spider_count])  # [BAT, SCORPION, SPIDER]
 
-
-
-def remove_duplicates(data):
-    unique_data = []
-    seen = []
-
-    for item in data:
-        key1 = item[0].flatten()
-        key2 = item[1].flatten()
-
-        # check duplicate
-        if not any(jnp.array_equal(key1, s[0]) and jnp.array_equal(key2, s[1]) for s in seen):
-            seen.append((key1, key2))
-            unique_data.append(item)
-
-    return unique_data
-
-
-def get_len_solutions(env_map: chex.Array,
-                      passable_tiles: chex.Array,
-                      src: chex.Array, key: chex.Array, trg: chex.Array,
-                      monster_label: chex.Array) -> Tuple[int, chex.Array]:
-    solutions = []
-    max_path_len = get_max_path_length_static(map_shape=env_map.shape)
-
-    env_map = jnp.array(env_map)
-    for _xy in jnp.argwhere(env_map == monster_label):
-        y, x = _xy
-
-        env_map = env_map.at[y, x].set(1)
-        pk_path_length, pk_flood_state, _ = calc_path_from_a_to_b(env_map, passable_tiles, src, key)
-        kd_path_length, kd_flood_state, _ = calc_path_from_a_to_b(env_map, passable_tiles, key, trg)
-        if pk_path_length > 0 and kd_path_length > 0:
-            whole_path = get_whole_path(pk_flood_state, kd_flood_state, max_path_len)
-            solutions.append(whole_path)
-
-    return env_map, solutions
-
 def get_whole_path(pk_flood_state: chex.Array, kd_flood_state: chex.Array, max_path_len: chex.Array) -> Tuple[int, chex.Array]:
     pk_flood_count = pk_flood_state.flood_count
     kd_flood_count = kd_flood_state.flood_count
@@ -742,3 +717,11 @@ def get_whole_path(pk_flood_state: chex.Array, kd_flood_state: chex.Array, max_p
     kd_coords = get_path_coords(kd_flood_count, max_path_len=max_path_len, coord1=kd_flood_state.trg)
 
     return jnp.array([pk_coords, kd_coords])
+
+def erase_unnecessary_arr(unprocess_arr: chex.Array):
+    arr = jnp.array(unprocess_arr, dtype=jnp.int32)
+
+    valid_mask = jnp.all(arr != jnp.array([-1, -1]), axis=1)
+    filtered_arr = arr[valid_mask]
+
+    return filtered_arr
