89705e3ec030a1c89ea15742704f1e1b7d53eee3
Baek In-Chang <inchang.baek@gm.gist.ac.kr>
2024-12-18 18:18:18 +0900

    update scenario fewshot reward function

--- pcgrllm/bypass_reward/fewshot/shape_2.py
+++ pcgrllm/bypass_reward/fewshot/shape_2.py
@@ -1,26 +1,55 @@
+import jax
 import jax.numpy as jnp
+import numpy as np
+from jax import jit
 
+from debug.render_level import render_level
+from envs.pathfinding import calc_path_from_a_to_b
+
+@jit
 def compute_reward(prev_array, unused3, curr_array, unused4) -> float:
     reward = 0.0
 
-    # Define target counts for the scenario
-    target_spiders = 3
-    target_doors = 1
+    EMPTY = 1
+    WALL = 2
+    PLAYER = 3
+    BAT = 4
+    SCORPION = 5
+    SPIDER = 6
+    KEY = 7
+    DOOR = 8
 
-    # Count the number of spiders and doors in the current array
-    curr_spiders = jnp.sum(curr_array == 6)
-    curr_doors = jnp.sum(curr_array == 8)
 
-    # Count the number of spiders and doors in the previous array
-    prev_spiders = jnp.sum(prev_array == 6)
-    prev_doors = jnp.sum(prev_array == 8)
+    def tile_loss(prev_array, curr_array, tile, tgt_count):
+        # Get the number of keys using jnp
+        prev_cnt = jnp.sum(prev_array == tile)
+        curr_cnt = jnp.sum(curr_array == tile)
 
-    # Calculate the difference in spider count and apply delta reward
-    spider_diff = abs(prev_spiders - target_spiders) - abs(curr_spiders - target_spiders)
-    reward += spider_diff * 0.5
+        prev_loss = abs(prev_cnt - tgt_count)
+        curr_loss = abs(curr_cnt - tgt_count)
+        key_loss = prev_loss - curr_loss
 
-    # Calculate the difference in door count and apply delta reward
-    door_diff = abs(prev_doors - target_doors) - abs(curr_doors - target_doors)
-    reward += door_diff * 1.0
+        return key_loss
+
+    passible_tiles = jnp.array([EMPTY, DOOR, PLAYER])
+
+    xy_player = jnp.argwhere(curr_array == PLAYER, size=1)
+    xy_door = jnp.argwhere(curr_array == DOOR, size=1)
+
+    key_loss = tile_loss(prev_array, curr_array, KEY, 1)
+    reward += key_loss * 1.0
+
+    prev_p_d_length, _, _ = calc_path_from_a_to_b(prev_array, passible_tiles, xy_player[0], xy_door[0])
+    curr_p_d_length, _, _ = calc_path_from_a_to_b(curr_array, passible_tiles, xy_player[0], xy_door[0])
+
+    is_better = jnp.bitwise_and(prev_p_d_length == -1, curr_p_d_length >= 0)
+    reward = jnp.where(is_better, reward + 3.0, reward)
+
+    bat_loss = tile_loss(prev_array, curr_array, BAT, 1)
+    scol_loss = tile_loss(prev_array, curr_array, SCORPION, 1)
+    spi_loss = tile_loss(prev_array, curr_array, SPIDER, 0)
+
+    reward += bat_loss * 1.0 + scol_loss * 1.0 + spi_loss * 0.5
 
     return reward
+
